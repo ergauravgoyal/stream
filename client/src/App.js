8da735f94df74d8f2bead2e65e8f5584321cc04a
Gaurav Goyal <gauravgoyal129@gmail.com>
2019-08-03 21:34:00 +0530

    Change for Stream Show

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Router, Route } from "react-router-dom";
+import { Router, Route, Switch } from "react-router-dom";
 import StreamList from "./components/streams/StreamList";
 import StreamCreate from "./components/streams/StreamCreate";
 import StreamEdit from "./components/streams/StreamEdit";
@@ -7,17 +7,21 @@ import StreamDelete from "./components/streams/StreamDelete";
 import StreamShow from "./components/streams/StreamShow";
 import Header from "./components/Header";
 import history from "../src/history";
+import NotFound from "./components/NotFound";
 
 const App = () => {
   return (
     <div className="ui container">
       <Router history={history}>
         <Header />
-        <Route path="/" exact component={StreamList} />
-        <Route path="/streams/new" component={StreamCreate} />
-        <Route path="/streams/edit/:id" component={StreamEdit} />
-        <Route path="/streams/delete/:id" component={StreamDelete} />
-        <Route path="/streams/show/:id" component={StreamShow} />
+        <Switch>
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit/:id" exact component={StreamEdit} />
+          <Route path="/streams/delete/:id" exact component={StreamDelete} />
+          <Route path="/streams/:id" exact component={StreamShow} />
+          <Route component={NotFound} />
+        </Switch>
       </Router>
     </div>
   );
